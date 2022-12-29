169ed3a0320b001e608b88302bc09cb8581923ef
Hanna L <hannalee92@gmail.com>
2022-12-29 15:07:58 -0500

    added fetch() for data retreival

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@
 //Initial Values
 const API_KEY = "89b3b7c5708d28b6ced951a4f58004eb"
 
+const url = https://api.themoviedb.org/3/search/movie?api_key=89b3b7c5708d28b6ced951a4f58004eb&query=furious
+
 //Selecting elements from the DOM
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
@@ -16,4 +18,14 @@ buttonElement.onclick = function(event) {
     event.preventDefault();
     const value = inputElement.value;
     console.log('Value: ', value);
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log('Data', data);
+        })
+        .catch(() => {
+            console.log('Error', error);
+        });
+
 }
