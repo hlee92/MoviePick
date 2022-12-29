191aa7e4743acfe8f3c1d4398472c88d3a5da2a4
Hanna L <hannalee92@gmail.com>
2022-12-29 13:32:27 -0500

    set up API Key

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 
+
+//Initial Values
+const API_KEY = "89b3b7c5708d28b6ced951a4f58004eb"
+
 //Selecting elements from the DOM
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
