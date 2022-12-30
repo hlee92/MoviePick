0efea991ac49f779330dbc2602f7fbb55945f15e
Hanna L <hannalee92@gmail.com>
2022-12-30 15:12:18 -0500

    added section and section img to css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,65 @@
 
 
 //Initial Values
-const API_KEY = "89b3b7c5708d28b6ced951a4f58004eb"
+const API_KEY = '89b3b7c5708d28b6ced951a4f58004eb';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
-const url = https://api.themoviedb.org/3/search/movie?api_key=89b3b7c5708d28b6ced951a4f58004eb&query=furious
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=89b3b7c5708d28b6ced951a4f58004eb';
 
 //Selecting elements from the DOM
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
+const movieSearchable = document.querySelector('#movies-searchable');
 
 
 
 
+function movieSection(movies) {
+    return movies.map((movie) => {
+        return `
+            <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
+        `;
+    })
+}
+
+function createMovieContainer(movies) {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const movieTemplate = `
+    <section class="section">
+        ${movieSection(movies)}
+        </section>
+        <div class="content">
+            <p id="content-close">X</p>
+        </div>
+    `;
+
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+
+}
 
 
-buttonElement.onclick = function(event) {
+buttonElement.onclick = function (event) {
     event.preventDefault();
     const value = inputElement.value;
-    console.log('Value: ', value);
 
-    fetch(url)
+    const newUrl = url + '&query=' + value;
+
+    fetch(newUrl)
         .then((res) => res.json())
         .then((data) => {
-            console.log('Data', data);
+            //data.results []
+            const movies = data.results;
+            const movieBlock= createMovieContainer(movies);
+            movieSearchable.appendChild(movieBlock);
+            console.log('Data: ', data);
         })
-        .catch(() => {
-            console.log('Error', error);
+        .catch((error) => {
+            console.log('Error: ', error);
         });
+
+    console.log('Value: ', value);
 
 }
