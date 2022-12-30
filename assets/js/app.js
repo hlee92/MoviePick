056ecef04c414824fab7f0807c931069ab3754d5
Hanna L <hannalee92@gmail.com>
2022-12-30 16:55:21 -0500

    generated path on where you want the url to go, url request, created function that generates the url based on the path making it more dynamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,14 +11,20 @@ const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
 const movieSearchable = document.querySelector('#movies-searchable');
 
-
-
+//makes generating the url based on the path, making it much more dynamic 
+function generateUrl(path) {
+    const url = `https://api.themoviedb.org/3${path}?api_key=89b3b7c5708d28b6ced951a4f58004eb`;
+    return url;
+}
 
 function movieSection(movies) {
     return movies.map((movie) => {
-        return `
-            <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
-        `;
+        if (movie.poster_path) {
+            return `<img 
+            src=${IMAGE_URL + movie.poster_path} 
+            data-movie-id=${movie.id}
+            />`;
+        }
     })
 }
 
@@ -30,7 +36,7 @@ function createMovieContainer(movies) {
     <section class="section">
         ${movieSection(movies)}
         </section>
-        <div class="content">
+        <div class="content content-display">
             <p id="content-close">X</p>
         </div>
     `;
@@ -40,26 +46,67 @@ function createMovieContainer(movies) {
 
 }
 
+function renderSearchMovies(data) {
+    //data.results []
+    movieSearchable.innerHTML = '';
+    const movies = data.results;
+    const movieBlock = createMovieContainer(movies);
+    movieSearchable.appendChild(movieBlock);
+    console.log('Data: ', data);
+}
+
 
 buttonElement.onclick = function (event) {
     event.preventDefault();
     const value = inputElement.value;
-
-    const newUrl = url + '&query=' + value;
+    const path = '/search/movie';
+    const newUrl = generateUrl(path) + '&query=' + value;;
 
     fetch(newUrl)
         .then((res) => res.json())
-        .then((data) => {
-            //data.results []
-            const movies = data.results;
-            const movieBlock= createMovieContainer(movies);
-            movieSearchable.appendChild(movieBlock);
-            console.log('Data: ', data);
-        })
+        .then(renderSearchMovies)
         .catch((error) => {
             console.log('Error: ', error);
         });
 
+    //Resets search value, clears old text
+    inputElement.value = '';
+
     console.log('Value: ', value);
 
+}
+//Event Delegation
+document.onclick = function (event) {
+
+    const target = event.target;
+
+    //When you click on any image, you are getting the movie id*
+    if (target.tagName.toLowerCase() === 'img') {
+        const movieId = target.dataset.movieId; //movie id*
+        console.log('Movie ID:', movieId);
+        const section = event.target.parentElement; //section in html
+        const content = section.nextElementSibling; //targetting content since it is under section in html
+        content.classList.add('content-display');
+
+        //Fetch movie videos
+        const path = `/movie/${movieId}/videos`; //generating the path where you want the url to go
+        const url = generateUrl(path); //end up having the url of where we want to make the request 
+        
+        fetch(url)
+            .then((res) => res.json())  //then we call the API which we get the movie back 
+            .then((data) => {
+                //TO DO
+                //Display movie videos
+                console.log('Videos:', data);
+            })
+            .catch((error) => {
+                console.log('Error: ', error);
+            });
+
+    }
+
+    if (target.id === 'content-close') {
+        const content = target.parentElement;
+        content.classList.remove('content-display');
+    }
 }
